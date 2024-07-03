8b8f0f1a949f96d898444faa407aea6c9bfb14ee
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-07-03 15:35:17 +0100

    Initial Commit

---
+++ Hot Prospects/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Hot Prospects
+//
+//  Created by Mehmet Alp Sönmez on 02/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var output = ""
+    
+    var body: some View {
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+    
+    func fetchReadings() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Error \(error.localizedDescription)"
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Mehmet Alp Sönmez on 02/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
