d86557805c4217430e80f9e6f016737a132dddcd
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-07-03 16:10:44 +0100

    Custom Swip Action

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -8,34 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var output = ""
+    @State private var backgroundColour = Color.red
     
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
-    }
-    
-    func fetchReadings() async {
-        let fetchTask = Task {
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
-        }
-        
-        let result = await fetchTask.result
-        
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "Error \(error.localizedDescription)"
+        List {
+            Text("Taylor Swift")
+                .swipeActions {
+                    Button("Delete", systemImage: "minus.circle", role: .destructive) {
+                        print("Delete")
+                    }
+                    Button("Message", systemImage: "message") {
+                        print("Hi")
+                    }
+                }
+                
+                    .swipeActions(edge: .leading) {
+                        Button("Pin", systemImage: "pin") {
+                            print("Pinning")
+                        }
+                        .tint(.orange)
+                    }
+                }
         }
     }
-}
+
 
 #Preview {
     ContentView()
